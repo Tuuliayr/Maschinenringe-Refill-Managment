10763028fc2ed8e85e6e0478c1abcfa3fc7ebc66
Tuuli <tuuli.ayravainen@tuni.fi>
2021-11-17 16:14:41 +0200

    Delete default header.

--- meine-box/src/App.js
+++ meine-box/src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components';
-import { AmplifySignOut } from '@aws-amplify/ui-react';
+import React from "react";
+import { AuthState, onAuthUIStateChange } from "@aws-amplify/ui-components";
+import { AmplifySignOut } from "@aws-amplify/ui-react";
 
 import Authentication from "./components/organisms/authentication/Authentication";
 import ProductsOverview from "./components/molecules/overview/ProductsOverview";
@@ -12,28 +12,16 @@ function App() {
   const [user, setUser] = React.useState();
 
   React.useEffect(() => {
-      return onAuthUIStateChange((nextAuthState, authData) => {
-          setAuthState(nextAuthState);
-          setUser(authData);
-      });
+    return onAuthUIStateChange((nextAuthState, authData) => {
+      setAuthState(nextAuthState);
+      setUser(authData);
+    });
   }, []);
-  
+
   return authState === AuthState.SignedIn && user ? (
     <div className="App">
       <div>Hello, {user.username}</div>
       <AmplifySignOut />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Welcome to Meine Box. This is our test branch!</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <ProductsOverview />
     </div>
   ) : (
