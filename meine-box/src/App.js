49325f54e2fd5be8ac976455cff347c7cb8e2352
haelina <56834622+haelina@users.noreply.github.com>
2021-11-17 11:08:47 +0200

    Install aws-amplify and add imports for graphql

--- meine-box/src/App.js
+++ meine-box/src/App.js
@@ -1,23 +1,29 @@
-import React from 'react';
-import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components';
-import { AmplifySignOut } from '@aws-amplify/ui-react';
+import React from "react";
+import { AuthState, onAuthUIStateChange } from "@aws-amplify/ui-components";
+import { AmplifySignOut } from "@aws-amplify/ui-react";
 
 import Authentication from "./components/organisms/authentication/Authentication";
 import ProductsOverview from "./components/molecules/overview/ProductsOverview";
 import logo from "./logo.svg";
 import "./Styles.scss";
 
+import Amplify, { API, graphqlOperation } from "aws-amplify";
+import awsconfig from "./aws-exports";
+import * as queries from "./graphql/queries";
+
+Amplify.configure(awsconfig);
+
 function App() {
   const [authState, setAuthState] = React.useState();
   const [user, setUser] = React.useState();
 
   React.useEffect(() => {
-      return onAuthUIStateChange((nextAuthState, authData) => {
-          setAuthState(nextAuthState);
-          setUser(authData);
-      });
+    return onAuthUIStateChange((nextAuthState, authData) => {
+      setAuthState(nextAuthState);
+      setUser(authData);
+    });
   }, []);
-  
+
   return authState === AuthState.SignedIn && user ? (
     <div className="App">
       <div>Hello, {user.username}</div>
