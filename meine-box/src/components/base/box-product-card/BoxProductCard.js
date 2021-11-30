138cd88cdeddca69ab4604d2e5890d3dad00a3d7
Jennina Färm <jennina.farm@exove.fi>
2021-11-30 15:55:33 +0200

    Add box product card styles and logic to define icons

--- meine-box/src/components/base/box-product-card/BoxProductCard.js
+++ meine-box/src/components/base/box-product-card/BoxProductCard.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-const BoxProductCard = ({name, quantity, unit}) => {
+const BoxProductCard = ({name, quantity, lowStock, unit}) => {
+  const [icon, setIcon] = useState("check");
+
+  useEffect(() => {
+    if(quantity === 0) {
+      setIcon("danger");
+    } else if (quantity <= lowStock) {
+      setIcon("low");
+    }
+  }, [quantity, setIcon, lowStock]);
+
   return (
     <div className="box-product-card">
-      <div className="box-product-card__icon">icon</div>
-      <div className="box-product-card__name">{name}</div>
+      <div className="box-product-card__title">
+        <div className="box-product-card__icon">{icon}</div>
+        <div className="box-product-card__name">{name}</div>
+      </div>
       <div className="box-product-card__stock">{quantity}{unit} in stock</div>
     </div>
   );
