a7a68d50da34386ecf675fd315b89b06004b4eec
Jennina Färm <jennina.farm@exove.fi>
2021-12-02 14:09:31 +0200

    Add links, icons and styling for the menu

--- meine-box/src/components/organisms/navigation/Navigation.js
+++ meine-box/src/components/organisms/navigation/Navigation.js
@@ -1,8 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { NavLink, useParams , useLocation} from "react-router-dom";
+
+import {ReactComponent as Home} from "../../../images/icons/store-alt-solid.svg";
+import {ReactComponent as Menu} from "../../../images/icons/bars-solid.svg";
 
 const Navigation = () =>  {
+  const [location, setLocation] = useState();
+  const [home, setHome] = useState("");
+
+  const loc = useLocation();
+    console.log(loc.pathname);
+
+  useEffect(() => {
+    setLocation(loc.pathname)
+  });
+
+  useEffect(() => {
+    if(location === "/") {
+      setHome("open");
+    } else {
+      setHome("");
+    }
+  }, [location]);
+
   return (
-    <div className="navigation">Navigation</div>
+    <div className="navigation">
+      <NavLink to="/" > 
+        <Home className={`navigation__icon navigation__icon--${home}`}/>
+      </ NavLink>
+      <NavLink to="/">
+        <Menu className={"navigation__icon navigation__icon--burger"} />
+      </NavLink>
+    </div>
   );
 }
 
