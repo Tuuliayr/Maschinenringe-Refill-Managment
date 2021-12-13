a535eb804b2273a2d6783d1a7b0a348db631260c
Tuuli <tuuli.ayravainen@tuni.fi>
2021-12-13 15:56:27 +0200

    Remove useEffect import.

--- meine-box/src/components/molecules/notifications/ExpertNotification.js
+++ meine-box/src/components/molecules/notifications/ExpertNotification.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Button from "../../base/buttons/ButtonBase";
 import NotificationCard from "../../base/notification-card/NotificationCard";
 
