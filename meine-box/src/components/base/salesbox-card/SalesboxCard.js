358315cb56029f145a4e750c3c9b7806c685bd70
Tuuli <tuuli.ayravainen@tuni.fi>
2021-12-13 13:55:30 +0200

    .

--- meine-box/src/components/base/salesbox-card/SalesboxCard.js
+++ meine-box/src/components/base/salesbox-card/SalesboxCard.js
@@ -1,14 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
-import BoxProductCard from '../box-product-card/BoxProductCard';
+import BoxProductCard from "../box-product-card/BoxProductCard";
 import * as dbData from "../../organisms/databaseconnection/DatabaseConnection";
 
-const SalesboxCard = ({boxId, boxProducts}) => {
+const SalesboxCard = ({ boxId, boxProducts }) => {
   const [products, setProducts] = useState([]);
   const [address, setAddress] = useState("");
 
   useEffect(() => {
-    if(boxProducts !== undefined) {
+    if (boxProducts !== undefined) {
       setProducts(boxProducts);
     }
   }, [boxProducts]);
@@ -17,31 +17,30 @@ const SalesboxCard = ({boxId, boxProducts}) => {
     const fetchBox = async () => {
       const salesbox = await dbData.getSalesboxdata(boxId);
       setAddress(salesbox.street_address);
-    }
+    };
 
-    if(boxId !== undefined && address === "") {
+    if (boxId !== undefined && address === "") {
       fetchBox();
     }
-  }) 
+  });
 
   return (
     <NavLink className="link" to={`/productsoverview/${boxId}`}>
-      <div className="box-card"> 
+      <div className="box-card">
         <h3 className="box-card__title"> Box {boxId} </h3>
         <p className="box-card__address">{address}</p>
-        {products.map(product => (
+        {products.map((product) => (
           <BoxProductCard
             key={product.id}
             name={product.name}
             quantity={product.stock_quantity}
             lowStock={product.low_stock_definition}
             unit={product.unit_value}
-
           />
         ))}
       </div>
     </NavLink>
   );
-}
+};
 
 export default SalesboxCard;
